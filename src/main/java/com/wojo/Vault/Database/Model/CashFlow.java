e55976d49f0cf53d035787f4a29286154e3fddb9
Wojciech Koziol <7wojok7@gmail.com>
2018-02-25 23:32:32 +0100

    add model

---
+++ src/main/java/com/wojo/Vault/Database/Model/CashFlow.java
@@ -0,0 +1,49 @@
+package com.wojo.Vault.Database.Model;
+
+import java.math.BigDecimal;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CashFlow {
+
+    private Month month;
+    private List<Payment> paymentList = new ArrayList<>();
+
+    public CashFlow(Month month) {
+        this.month = month;
+    }
+
+    public Month getMonth() {
+        return month;
+    }
+
+    public boolean addPayment(Payment payment) {
+        return paymentList.add(payment);
+    }
+
+    public BigDecimal getDeposit() {
+        return BigDecimal.valueOf(
+                paymentList.stream()
+                        .filter(payment -> payment.getPaymentValue()
+                                .compareTo(BigDecimal.ZERO) > 0)
+                        .mapToDouble(i -> Double.valueOf(i.getPaymentValue().toString()))
+                        .sum());
+    }
+
+    public BigDecimal getDebit() {
+        return BigDecimal.valueOf(
+                paymentList.stream()
+                        .filter(payment -> payment.getPaymentValue()
+                                .compareTo(BigDecimal.ZERO) < 0)
+                        .mapToDouble(i -> Double.valueOf(i.getPaymentValue().toString()))
+                        .sum());
+    }
+
+    public BigDecimal getSum() {
+        return BigDecimal.valueOf(
+                paymentList.stream()
+                        .mapToDouble(i -> Double.valueOf(i.getPaymentValue().toString()))
+                        .sum());
+    }
+}
