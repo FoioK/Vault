34aff08a9a2891e54492d942d6f36c7e9873816a
Wojciech Koziol <7wojok7@gmail.com>
2018-03-10 19:50:10 +0100

    DepositDAO: add getAllDeposits method.

--- src/main/java/com/wojo/Vault/Database/DAO/DepositDAO.java
+++ src/main/java/com/wojo/Vault/Database/DAO/DepositDAO.java
@@ -2,7 +2,11 @@ package com.wojo.Vault.Database.DAO;
 
 import com.wojo.Vault.Database.Model.Deposits;
 
+import java.util.List;
+
 public interface DepositDAO {
 
     <T extends Deposits> Integer insertDepositToDB(T deposit);
+
+    List<Deposits> getAllDeposits(Integer idAccount);
 }
--- src/main/java/com/wojo/Vault/Database/DAO/Impl/DepositDAOImpl.java
+++ src/main/java/com/wojo/Vault/Database/DAO/Impl/DepositDAOImpl.java
@@ -3,9 +3,17 @@ package com.wojo.Vault.Database.DAO.Impl;
 import com.wojo.Vault.Database.DAO.DepositDAO;
 import com.wojo.Vault.Database.DBManager;
 import com.wojo.Vault.Database.Model.Deposits;
+import com.wojo.Vault.Database.Model.DepositsModel.LongDeposit;
+import com.wojo.Vault.Database.Model.DepositsModel.MiddleDeposit;
+import com.wojo.Vault.Database.Model.DepositsModel.ShortDeposit;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class DepositDAOImpl implements DepositDAO {
 
@@ -26,5 +34,40 @@ public class DepositDAOImpl implements DepositDAO {
             e.printStackTrace();
             return 0;
         }
+    }
+
+    @Override
+    public List<Deposits> getAllDeposits(Integer idAccount) {
+        String queryStatement = "SELECT * FROM deposit WHERE idAccount = ?";
+        List<Deposits> allDeposits = new ArrayList<>();
+        try {
+            ResultSet resultSet = DBManager.dbExecuteQuery(queryStatement,
+                    Collections.singletonList(String.valueOf(idAccount)));
+            while (resultSet.next()) {
+
+                String type = resultSet.getString("TYPE");
+                BigDecimal depositAmount = resultSet.getBigDecimal("AMOUNT");
+                LocalDateTime startDate =
+                        LocalDateTime.ofInstant(
+                                Instant.ofEpochMilli(resultSet.getTimestamp("DATE_START").getTime()),
+                                TimeZone.getDefault().toZoneId());
+
+                if (type.equals(Deposits.DepositType.Short.toString())) {
+                    allDeposits.add(new ShortDeposit(idAccount, depositAmount, startDate));
+                } else if (type.equals(Deposits.DepositType.Middle.toString())) {
+                    allDeposits.add(new MiddleDeposit(idAccount, depositAmount, startDate));
+                } else if (type.equals(Deposits.DepositType.Long.toString())) {
+                    allDeposits.add(new LongDeposit(idAccount, depositAmount, startDate));
+                } else {
+                    return new ArrayList<>();
+                }
+            }
+            return allDeposits;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>();
     }
 }
