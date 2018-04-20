c9b08a51d322c5bc6719405291fb4892edee44ed
Wojciech Koziol <7wojok7@gmail.com>
2018-04-20 11:19:22 +0200

    Add Configuration to payment tests.

---
+++ src/test/java/com/wojo/Vault/Service/impl/Configuration.java
@@ -0,0 +1,30 @@
+package com.wojo.Vault.Service.impl;
+
+import com.wojo.Vault.Database.DBManager;
+import com.wojo.Vault.Exception.ExecuteStatementException;
+
+public class Configuration {
+
+    public static void connectionToTestDatabase() {
+        DBManager.setTestConnectionPath();
+        DBManager.dbConnection();
+    }
+
+    public static void disableForeignKeyCheck() throws ExecuteStatementException {
+        final String disableKeyCheck = "SET FOREIGN_KEY_CHECKS = 0";
+
+        DBManager.dbExecuteUpdate(disableKeyCheck, null);
+    }
+
+    public static void enableForeignKeyCheck() throws ExecuteStatementException {
+        final String enableKeyCheck = "SET FOREIGN_KEY_CHECKS = 1";
+
+        DBManager.dbExecuteUpdate(enableKeyCheck, null);
+    }
+
+    public static void truncateTable(String tableName) throws ExecuteStatementException {
+        final String truncateTable = "TRUNCATE TABLE " + tableName;
+
+        DBManager.dbExecuteUpdate(truncateTable, null);
+    }
+}
