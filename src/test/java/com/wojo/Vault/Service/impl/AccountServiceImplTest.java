2f66a4d42acb1df5fb3370ddc8683dda451029c8
Wojciech Koziol <7wojok7@gmail.com>
2018-05-10 23:00:34 +0200

    Refactor AccountService.

--- src/test/java/com/wojo/Vault/Service/impl/AccountServiceImplTest.java
+++ src/test/java/com/wojo/Vault/Service/impl/AccountServiceImplTest.java
@@ -1,35 +1,121 @@
 package com.wojo.Vault.Service.impl;
 
+import com.wojo.Vault.Database.DBManager;
 import com.wojo.Vault.Database.Model.Account;
 import com.wojo.Vault.Database.Model.Person;
+import com.wojo.Vault.Exception.ExecuteStatementException;
 import com.wojo.Vault.Service.AccountService;
-import org.junit.Test;
+import org.junit.*;
 
 import java.math.BigDecimal;
-import java.util.Collections;
+import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AccountServiceImplTest {
+
+    private static final String ACCOUNT_TABLE_NAME = "Account";
+
+    @BeforeClass
+    public static void setup() throws ExecuteStatementException {
+        Configuration.connectionToTestDatabase();
+        Configuration.disableForeignKeyCheck();
+        Configuration.truncateTable(ACCOUNT_TABLE_NAME);
+    }
+
+    @AfterClass
+    public static void clearDatabaseAndDisconnect() throws ExecuteStatementException {
+        Configuration.disableForeignKeyCheck();
+        Configuration.truncateTable(ACCOUNT_TABLE_NAME);
+        Configuration.enableForeignKeyCheck();
+        DBManager.dbDisconnect();
+    }
+
+
+    private static final Account ACCOUNT_TO_TESTS = new Account(
+            "11",
+            "6",
+            "43215678901234567890123456",
+            new BigDecimal("9000.00")
+    );
+
+    @Before
+    public void insertDataToTest() throws ExecuteStatementException {
+        assertTrue(insertAccount());
+    }
+
+    private boolean insertAccount() throws ExecuteStatementException {
+        String updateStatement = "INSERT INTO account (ACCOUNT_ID, PERSON_ID, NUMBER, VALUE) " +
+                "VALUES (?, ?, ?, ?)";
+
+        return DBManager.dbExecuteUpdate(updateStatement, Arrays.asList(
+                AccountServiceImplTest.ACCOUNT_TO_TESTS.getAccountId(),
+                AccountServiceImplTest.ACCOUNT_TO_TESTS.getPersonId(),
+                AccountServiceImplTest.ACCOUNT_TO_TESTS.getNumber(),
+                AccountServiceImplTest.ACCOUNT_TO_TESTS.getValue().toString()
+        )) == 1;
+    }
+
+    @After
+    public void clearTable() throws ExecuteStatementException {
+        Configuration.disableForeignKeyCheck();
+        Configuration.truncateTable(ACCOUNT_TABLE_NAME);
+    }
 
     private AccountService accountService = new AccountServiceImpl();
 
     @Test
-    public void shouldReturnFormattedNumber() {
-        String accountNumber = "PL12345678901234567890123496";
-        String formattedAccountNumber = "12 3456 7890 1234 5678 9012 3496";
+    public void shouldCreateAccount() {
+        Account account = new Account(
+                "10",
+                "5",
+                "12345678901234567890123456",
+                new BigDecimal("9000.00")
+        );
 
-        Account account = new Account();
-        account.setIBAN_NUMBER(accountNumber);
-        Person.setAccounts(Collections.singletonList(account));
-
-        assertEquals(formattedAccountNumber, accountService.getFormatAccountNumber());
+        assertTrue(accountService.createAccount(account));
     }
 
     @Test
-    public void formattedNumberShouldHaveLength32() {
-        Person.setAccounts(Collections.singletonList(new Account("PL", 26, BigDecimal.ZERO)));
-        assertEquals(32, accountService.getFormatAccountNumber().length());
+    public void shouldNotCreateAccount() {
+        assertFalse(accountService.createAccount(null));
     }
 
+    @Test
+    public void shouldAddValueToAccount() {
+        BigDecimal valueToAdd = new BigDecimal("1000.00");
+
+        assertTrue(accountService.addValue(valueToAdd, ACCOUNT_TO_TESTS.getNumber()));
+    }
+
+    @Test
+    public void shouldReturnFormattedNumber() {
+        assertEquals(ACCOUNT_TO_TESTS.getNumber(),
+                accountService.getFormatAccountNumber(ACCOUNT_TO_TESTS)
+                        .chars()
+                        .filter(c -> c != 32)
+                        .collect(StringBuilder::new,
+                                StringBuilder::appendCodePoint,
+                                StringBuilder::append)
+                        .toString());
+    }
+
+    @Test
+    public void formattedNumberShouldHave33CharacterLength() {
+        final int expectedLength = 33;
+        assertEquals(expectedLength, accountService.getFormatAccountNumber(ACCOUNT_TO_TESTS).length());
+    }
+
+    @Test
+    public void shouldSetAccounts() {
+        Person person = new Person(ACCOUNT_TO_TESTS.getPersonId());
+
+        accountService.setAccounts(person);
+
+        assertEquals(ACCOUNT_TO_TESTS,
+                person.getAccountList()
+                        .stream()
+                        .filter(account -> account.getPersonId().equals(ACCOUNT_TO_TESTS.getPersonId()))
+                        .findFirst().orElse(null));
+    }
 }
