e75e3eec86e7b2ff6d5e9138bf301daada62ed09
Wojciech Kozioł <7wojok7@gmail.com>
2018-07-18 22:00:30 +0200

    (Role, User, UserRole) entity.

---
+++ src/main/java/com/wojo/Vault/model/User.java
@@ -0,0 +1,65 @@
+package com.wojo.Vault.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private byte[] login;
+
+    @Column(nullable = false, length = 128)
+    private byte[] password;
+
+    @Column(nullable = false)
+    private LocalDateTime createTime;
+
+    @OneToMany(mappedBy = "user")
+    private Set<UserRole> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getLogin() {
+        return login;
+    }
+
+    public void setLogin(byte[] login) {
+        this.login = login;
+    }
+
+    public byte[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(byte[] password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/com/wojo/Vault/model/UserRole.java
@@ -0,0 +1,41 @@
+package com.wojo.Vault.model;
+
+import javax.persistence.*;
+
+@Entity
+public class UserRole {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
