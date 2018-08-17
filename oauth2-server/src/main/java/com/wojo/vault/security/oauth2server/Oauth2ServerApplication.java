c1f8b3230b3b9c194690c69789f98faf1aadca91
Wojciech Kozioł <7wojok7@gmail.com>
2018-08-17 23:29:40 +0200

    EnableAuthorizationServer

--- oauth2-server/src/main/java/com/wojo/vault/security/oauth2server/Oauth2ServerApplication.java
+++ oauth2-server/src/main/java/com/wojo/vault/security/oauth2server/Oauth2ServerApplication.java
@@ -2,8 +2,10 @@ package com.wojo.vault.security.oauth2server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class Oauth2ServerApplication {
 
     public static void main(String[] args) {
