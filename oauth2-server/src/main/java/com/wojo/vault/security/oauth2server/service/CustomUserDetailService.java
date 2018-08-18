cd391f4586cfb6064366529279898ecba312431c
Wojciech Kozioł <7wojok7@gmail.com>
2018-08-18 19:43:31 +0200

    Custom user detail service

---
+++ oauth2-server/src/main/java/com/wojo/vault/security/oauth2server/service/CustomUserDetailService.java
@@ -0,0 +1,51 @@
+package com.wojo.vault.security.oauth2server.service;
+
+import com.wojo.vault.security.oauth2server.model.CustomUserDetail;
+import com.wojo.vault.security.oauth2server.model.UserEntity;
+import com.wojo.vault.security.oauth2server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("User login not found!"));
+
+        if (Objects.equals(userEntity.getUserType(), "super_admin")) {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("SUPER_ADMIN");
+            userEntity.setGrantedAuthorityList(Collections.singletonList(grantedAuthority));
+        } else {
+            Collection<GrantedAuthority> permissions = new ArrayList<>();
+            userRepository.getPermissions(login)
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .forEach(permissions::add);
+
+            userEntity.setGrantedAuthorityList(permissions);
+        }
+
+        return new CustomUserDetail(userEntity);
+    }
+}
