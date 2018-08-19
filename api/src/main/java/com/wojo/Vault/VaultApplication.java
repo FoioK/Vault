8641d8061f36c44dfae3279f41eea99c1be3d3b6
Wojciech Kozioł <7wojok7@gmail.com>
2018-08-19 14:33:41 +0200

    Enable resource server

--- api/src/main/java/com/wojo/Vault/VaultApplication.java
+++ api/src/main/java/com/wojo/Vault/VaultApplication.java
@@ -2,8 +2,10 @@ package com.wojo.Vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class VaultApplication {
 
 	public static void main(String[] args) {
