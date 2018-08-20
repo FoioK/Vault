4226ccb9ce6c9214327bdfd73631378a58f7dda0
Wojciech Kozioł <7wojok7@gmail.com>
2018-08-20 23:27:11 +0200

    Login page

--- ui/src/app/component/login/login.component.ts
+++ ui/src/app/component/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {OAuthService} from "../../service/o-auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [OAuthService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private loginService: OAuthService) {
 
-  ngOnInit() {
   }
 
+  loginForm: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  getAccessToken() {
+    this.loginService.getToken(
+      this.loginForm.get('login').value,
+      this.loginForm.get('password').value)
+      .subscribe(respone => console.log(respone));
+  }
 }
