143d0868467ffef620419c94cb1de07a1a68188b
Wojciech Kozioł <7wojok7@gmail.com>
2020-04-19 23:10:55 +0200

    postgresql schema scripts

--- src/main/resources/db/postgresql-schema.sql
+++ src/main/resources/db/postgresql-schema.sql
@@ -1,12 +1,49 @@
-CREATE SCHEMA IF NOT EXISTS vault
+CREATE TABLE Person
+(
+    PERSON_ID        serial NOT NULL PRIMARY KEY,
+    FIRST_NAME       varchar(255),
+    LAST_NAME        varchar(255),
+    TELEPHONE_NUMBER varchar(255),
+    LOGIN            varchar(255),
+    PASSWORD         varchar(255),
+    CREATE_TIME      timestamp
+);
 
-    CREATE TABLE Person
-    (
-        PERSON_ID        serial NOT NULL PRIMARY KEY,
-        FIRST_NAME       varchar(255),
-        LAST_NAME        varchar(255),
-        TELEPHONE_NUMBER varchar(255),
-        LOGIN            varchar(255),
-        PASSWORD         varchar(255),
-        CREATE_TIME      timestamp
-    )
+CREATE TABLE Account
+(
+    ACCOUNT_ID serial NOT NULL PRIMARY KEY,
+    PERSON_ID  varchar(255),
+    NUMBER     varchar(255),
+    VALUE      numeric
+);
+
+CREATE TABLE Payment
+(
+    PAYMENT_ID           serial NOT NULL PRIMARY KEY,
+    SENDER_ACCOUNT_ID    varchar(255),
+    RECIPIENT_ACCOUNT_ID varchar(255),
+    RECIPIENT_NAME       varchar(255),
+    RECIPIENT_NUMBER     varchar(255),
+    AMOUNT               numeric,
+    TITLE                varchar(255),
+    CREATE_TIME          timestamp
+);
+
+CREATE TABLE Address
+(
+    ADDRESS_ID       serial NOT NULL PRIMARY KEY,
+    CITY             varchar(255),
+    STREET           varchar(255),
+    APARTMENT_NUMBER varchar(255)
+);
+
+CREATE TABLE Address
+(
+    DEPOSIT_ID serial NOT NULL PRIMARY KEY,
+    ACCOUNT_ID varchar(255),
+    AMOUNT     numeric,
+    START_DATE timestamp,
+    END_DATE   timestamp,
+    TYPE       varchar(255),
+    IS_ACTIVE  varchar(255)
+);
